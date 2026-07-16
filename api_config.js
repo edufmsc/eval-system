cbb4b43ed06f73db12c09c979970397792cf3461
edufmsc <edu.fmsc@gmail.com>
2026-07-16 10:46:33 +0800

    Update comments and variable names in api_config.js

--- api_config.js
+++ api_config.js
@@ -2,10 +2,10 @@
 // 前端模組 1：API 通訊設定與全域中央狀態快取大腦
 // =================================================================
 
-// 🌟 未來後台如果重新發布部署，您只需要置換這行引號內的網址，其他任何檔案都不必動！
+// 🌟 未來後台大腦重新部署換新網址時，您只需要置換這行引號內的網址，其他任何檔案都不必動！
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbysdFVEq81YuSme1wp72i_MePw2P2TkGPysYJ1gJEvC-58DTuzCkyZbwMddqjiBMTuJ/exec";
 
-// 全域跨模組共享中央快取變數
+// 全域跨模組共享中央記憶體變數
 let currentUser = null;
 let subordinateCache = [];
 let pendingFormCache = [];
